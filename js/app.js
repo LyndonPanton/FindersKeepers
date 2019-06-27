b8c7f421392acad9fe9f3374bcfbb8423ceb4ab3
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 15:51:13 +0100

    Added logic for displaying result
    The numbers in the array, the condition, the first number in the array which passed the condition

--- js/app.js
+++ js/app.js
@@ -3,9 +3,27 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	// Edit me
+	let arr = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+	function condition(number) {
+		// Edit me
+		if (number % 5 === 0) {
+			return number;
+		} else {
+			return undefined;
+		}
+	}
+
+	function display(array) {
+		document.getElementById("display-array-values").textContent = array[0];
+		document.getElementById("display-condition-value").textContent = array[1];
+		document.getElementById("display-first-value").textContent = array[2];
+	}
+
 	function find(arr, func) {
 		// Method I
-		return [arr, func, arr.filter(func)[0]];
+		return display([arr, func, arr.filter(func)[0]]);
 
 		// Method II
 		// let value = undefined;
@@ -18,19 +36,7 @@ window.onload = function(event) {
 
 		// }
 
-		// return [arr, func, value];
-	}
-
-	// Edit me
-	let arr = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-
-	function condition(number) {
-		// Edit me
-		if (number % 5 === 0) {
-			return number;
-		} else {
-			return undefined;
-		}
+		// return display([arr, func, value]);
 	}
 
 	find(arr, condition);
