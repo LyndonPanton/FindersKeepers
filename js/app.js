58bb7c6e3d7cda2392cb182022968a67c3549649
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 15:42:38 +0100

    Added logic to find first number which passed the given test

--- js/app.js
+++ js/app.js
@@ -2,4 +2,34 @@
 
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function find(arr, func) {
+		// Method I
+		return [arr, func, arr.filter(func)[0]];
+
+		// Method II
+		// let value = undefined;
+
+		// for (let i = 0; i < arr.length; i++) {
+		// 	if (func(arr[i])) {
+		// 		value = arr[i];
+		// 		break;
+		// 	}
+
+		// }
+
+		// return [arr, func, value];
+	}
+
+	// Edit me
+	let arr = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+	find(arr, function(number) {
+		// Edit me
+		if (number % 5 === 0) {
+			return number;
+		} else {
+			return undefined;
+		}
+	});
 };
